ac3f53b6aa98b4de425f23f9292c89706dee0131
Max Seidenstücker <max.seidenstuecker@gmail.com>
2025-08-16 20:43:00 +0200

    clang-format settings and scripts

---
+++ clang_format_sources.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Applying clang-format
+
+echo Sources:
+FOR /F "tokens=*" %%g IN ('dir /s /b .\src\*.cpp') do (
+  call clang-format -i %%g
+  <NUL set /p dummyName="."
+  )
+echo.
+
+echo Headers:
+FOR /F "tokens=*" %%g IN ('dir /s /b .\src\*.h') do (
+  call clang-format -i %%g
+  <NUL set /p dummyName="."
+  )
+echo.
+
+echo.
+echo Done
