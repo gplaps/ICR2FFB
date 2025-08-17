57284a4aa85bd9e2b0a6f614d8c4682c24f7ac6b
Max Seidenstücker <max.seidenstuecker@gmail.com>
2025-08-17 21:41:12 +0200

    C++98 again

--- src/game_version.h
+++ src/game_version.h
@@ -1,6 +1,11 @@
 #pragma once
 
-enum GameVersion : unsigned char
+#include "project_dependencies.h"
+
+enum GameVersion
+#if defined(IS_CPP11_COMPLIANT)
+    : unsigned char
+#endif
 {
     VERSION_UNINITIALIZED,
     ICR2_DOS4G_1_02,
