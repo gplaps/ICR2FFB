d42fa2e32eef4ed18a4ff428b116d3eab8a189c2
Max Seidenstücker <max.seidenstuecker@gmail.com>
2025-08-18 02:45:59 +0200

    cppcheck works without corrective / additional includes with proper compile_commands.json

--- scripts/cppcheck_include.hpp
+++ scripts/cppcheck_include.hpp
@@ -1,4 +1,1 @@
 #pragma once
-#if !defined(_WIN32_WINNT)
-#define _WIN32_WINNT 0x501 // 0x500 like in project config results in mingw-std-threads errors, but only in cppcheck, not in project itself, haven't looked why, don't ask
-#endif
