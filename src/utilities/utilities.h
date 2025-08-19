9b8a85ed0f98814e3ae68d8ea31a3a6619f298ad
Max Seidenstücker <max.seidenstuecker@gmail.com>
2025-08-19 21:28:31 +0200

    Utils

--- src/utilities/utilities.h
+++ src/utilities/utilities.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define STRINGIFY_W(x)           L##x
+#define STRINGIFY_WIDE(x)        STRINGIFY_W(#x)
+#define STRINGIFY_WIDE_STRING(x) L"" STRINGIFY_W(x) L""
+
 // a function-like macro to check return code like FAILED/SUCCESS for HRESULT
 #define STATUS_CHECK(func) \
     res = (func);          \
